3b72b5839be49ff6de1e77f06c208b79fdfa7705
Sahil Latif <sahillatif072@gmail.com>
2024-07-13 12:51:59 +0500

    added all needed classes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
+#include<bits/stdc++.h>
 #include "raylib.h"
 
+using namespace std;
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+struct TypeColors{
+    Color type_int = GREEN, type_char = RED, type_float = ORANGE, type_string = BLUE, type_double = PURPLE, type_vector = YELLOW;
+};
+class Variable{
+    const string type, name;
+    string value;
+    const bool isConst, hasValue, isGlobal;
+    int rowNo;
+    TypeColors colors;
+    public:
+    Variable(string type, string name, bool isConst, bool hasValue, bool isGlobal, int rowNo):type(type), name(name), isConst(isConst), hasValue(hasValue), isGlobal(isGlobal), rowNo(rowNo){}
+    Variable(string type, string name, string value, bool isConst, bool hasValue, bool isGlobal, int rowNo):type(type), name(name), value(value), isConst(isConst), hasValue(hasValue), isGlobal(isGlobal), rowNo(rowNo){}
+    void setValue(const string val){value = val;}
+
+    void draw(){
+    }
+
+};
+class Function{
+
+};
+class Loop{
+
+};
+class Class{
+
+};
+class Structure{
+
+};
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Raylib Example - Basic Window");
 
